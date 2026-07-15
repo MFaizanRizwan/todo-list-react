fa91415f463c4db3d02bd54a499a116773f07896
MFaizanRizwan <mfaizanrizwan57@gmail.com>
2026-07-15 17:28:13 +0500

    Added the error handling

--- src/services/task_services/addTask.js
+++ src/services/task_services/addTask.js
@@ -1,7 +1,12 @@
 import { collection, addDoc } from "firebase/firestore";
 import { db } from "../firebase";
 
-export const addTask = async (task) => {
-  const docRef = await addDoc(collection(db, "tasks"), task);
-  return docRef.id;
+export const addTask = async (taskData) => {
+    try {
+        const docRef = await addDoc(tasksCollection, taskData);
+        return docRef.id;
+    } catch (error) {
+        console.error("Error adding task:", error);
+        throw error;
+    }
 };
