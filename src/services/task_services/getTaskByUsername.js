1a4551b35aed8e0d903fc9418b02794b9a818ce4
MFaizanRizwan <mfaizanrizwan57@gmail.com>
2026-07-20 16:37:34 +0500

    Fixed the task id bug

--- src/services/task_services/getTaskByUsername.js
+++ src/services/task_services/getTaskByUsername.js
@@ -1,13 +1,15 @@
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from "../firebase";
 
+const tasksCollection = collection(db, "tasks");
+
 export const getTasksByUser = async (username) => {
     try {
         const q = query(tasksCollection, where("author", "==", username));
         const querySnapshot = await getDocs(q);
 
         return querySnapshot.docs.map(doc => ({
-            firestoreId: doc.id,
+            id: doc.id,
             ...doc.data()
         }));
     } catch (error) {
